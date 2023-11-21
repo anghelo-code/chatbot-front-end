ef6b0a3fa7245ae1cfd2cf185cffeb7a9cd5c17b
Anghelo Alagon <200518@unsaac.edu.pe>
2023-11-21 12:17:45 -0500

    install Font Awesome and build HTML for the chatbot

--- src/components/chatbot/index.jsx
+++ src/components/chatbot/index.jsx
@@ -1,7 +1,10 @@
-export const chatbot = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+export const Chatbot = () => {
   return (
     <span>
-
+      <FontAwesomeIcon icon={ faWhatsapp }/>
     </span>
   )
 }
