381b388ec5b4eb2d3cfccd1ab7c431abfa5959fd
Anghelo Alagon <200518@unsaac.edu.pe>
2023-11-21 13:09:20 -0500

    add style for the chat button

--- src/components/chatbot/index.jsx
+++ src/components/chatbot/index.jsx
@@ -1,10 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faX, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+import './style.component.css';
 
 export const Chatbot = () => {
   return (
-    <span>
-      <FontAwesomeIcon icon={ faWhatsapp }/>
+    <span className='chatbot'>
+      <input type="checkbox" id="checkbox" className='chatbot-check' />
+      <label htmlFor="checkbox" className='chatbot-label' >
+        <FontAwesomeIcon icon={ faCommentAlt } className='chatbot-icon_chat' />
+        <FontAwesomeIcon icon={ faX } className='chatbot-icon_close' />
+      </label>
     </span>
   )
 }
