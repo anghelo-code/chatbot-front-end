c6e37b45fe3c22c6252b08f2a57211825ce5c47c
Anghelo Alagon <200518@unsaac.edu.pe>
2023-11-23 11:28:52 -0500

    add structure for the chatbot's body

--- src/components/chatbot/index.jsx
+++ src/components/chatbot/index.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faX, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+import { faX, faCommentAlt, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './style.component.css';
 
 export const Chatbot = () => {
@@ -11,6 +11,34 @@ export const Chatbot = () => {
         <FontAwesomeIcon icon={ faCommentAlt } className='chatbot-icon_chat' />
         <FontAwesomeIcon icon={ faX } className='chatbot-icon_close' />
       </label>
+
+      <div className="chatbot-chat">
+        <div className="chatbot-head">
+          chatbot
+        </div>
+
+        <div className="chatbot-body">
+          <div className="chatbot-body_user">
+            <div>
+              Hola cual es el nommbre del lauro
+            </div>
+          </div>
+
+          <div className="chatbot-body_bot">
+            <div>
+              Cual es tu consulta
+            </div>
+          </div>
+        </div>
+
+        <div  className='chatbot-form'>
+          <input type="text"  className='chatbot-form_input'/>
+          <button className='chatbot-form_button'>
+            <FontAwesomeIcon icon={ faArrowRight }   className='chatbot-form_icon'/>
+          </button>
+        </div>
+
+      </div>
     </span>
   )
 }
